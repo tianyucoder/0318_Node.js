935a527e74f17f8b77119f7e25cb6efd405ea92b
tianyu <0318@qq.com>
2019-06-28 09:37:55 +0800

    ajax

---
+++ day06(Ajax重要)/1.原生Ajax请求/server.js
@@ -0,0 +1,14 @@
+let express = require('express')
+
+let app = express()
+app.use(express.static('public'))
+
+app.get('/testGET',(request,response)=>{
+  console.log('testGET路由被调用了')
+  response.send('我是服务返回的GET请求的信息')
+})
+
+app.listen(3000,(err)=>{
+  if(!err) console.log('测试原生js发送Ajax-GET请求的地址是：http://localhost:3000/ajax_get.html')
+  else console.log(err)
+})
