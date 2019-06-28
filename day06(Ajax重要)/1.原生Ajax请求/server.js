769a94779e0835fee2e39d8e33ab81e354dae034
tianyu <0318@qq.com>
2019-06-28 11:52:18 +0800

    ajax-get

--- day06(Ajax重要)/1.原生Ajax请求/server.js
+++ day06(Ajax重要)/1.原生Ajax请求/server.js
@@ -3,13 +3,27 @@ let express = require('express')
 let app = express()
 app.use(express.static('public'))
 app.disable('x-powered-by')
+app.use(express.urlencoded({extended:true}))
+
 app.get('/testGET',(request,response)=>{
   console.log(request.query);
   console.log('testGET路由被调用了')
-  response.send('我是服务器返回的GET请求的信息')
+  response.send('我是服务器返回的GET请求的信息11111')
+})
+
+app.post('/testPOST',(request,response)=>{
+  console.log(request.query);
+  console.log(request.body);
+  console.log('testPOST路由被调用了')
+  response.send('我是服务器返回的POST请求的信息')
 })
 
 app.listen(3000,(err)=>{
-  if(!err) console.log('测试原生js发送Ajax-GET请求的地址是：http://localhost:3000/ajax_get.html')
-  else console.log(err)
+  if(!err) {
+    console.log('测试原生js发送Ajax-GET请求的地址是：http://localhost:3000/ajax_get.html')
+    console.log('测试原生js发送Ajax-POST请求的地址是：http://localhost:3000/ajax_post.html')
+  }
+  else {
+    console.log(err)
+  }
 })
